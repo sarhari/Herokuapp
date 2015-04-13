09d6810830b44cf33cef289885b0522918b80f3a
saravana <sarhari@gmail.com>
2015-04-13 19:20:25 +0530

    commiting admin changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,7 @@
 class AdminController < ApplicationController
+ def create
+  puts "pppppppraaaaaamsss #{params.inspect}"
+  @role = Role.find 9
+  @role.users.create(:email=>params["email"], :password=>params["password"], :password_confirmation=>params["password_confirmation"])
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'home#index'
-
+   match "admin/create" => "admin#create", as: :admin_create, via: [:get, :post]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
